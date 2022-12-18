295acd22bcead55cd3aa72700f481cd1c842c966
Taha6400 <113461712+Taha6400@users.noreply.github.com>
2022-12-18 21:49:59 +0330

    Update just python.py

--- just python.py
+++ just python.py
@@ -1,14 +1,42 @@
+from re import X
 import turtle
 
 wn = turtle.Screen()
+wn.title("My Game")
 wn.bgcolor("black")
+wn.setup(width=800, height=600)
 wn.tracer(0)
 
-# the player
-player = turtle.Turtle()
-player.color("white")
-player.shape("square")
+# Paddle A
 
-# main game loop
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("green")
+paddle_a.shapesize(stretch_wid=2.5, stretch_len=2.5)
+paddle_a.penup()
+paddle_a.goto(00, 0)
+
+# Function
+
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+
+
+# Main game loop
 while True:
     wn.update()
