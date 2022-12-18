4b2642e0044ee8aa3f21b1b18d11bb16bf41160b
Taha6400 <113461712+Taha6400@users.noreply.github.com>
2022-12-18 21:52:02 +0330

    Update just python.py

--- just python.py
+++ just python.py
@@ -2,32 +2,32 @@ from re import X
 import turtle
 
 wn = turtle.Screen()
-wn.title("My Game")
+wn.title("Python-Project")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
 # Paddle A
 
-paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("green")
-paddle_a.shapesize(stretch_wid=2.5, stretch_len=2.5)
-paddle_a.penup()
-paddle_a.goto(00, 0)
+player = turtle.Turtle()
+player.speed(0)
+player.shape("square")
+player.color("green")
+player.shapesize(stretch_wid=2.5, stretch_len=2.5)
+player.penup()
+player.goto(00, 0)
 
 # Function
 
 def paddle_a_up():
-    y = paddle_a.ycor()
+    y = player.ycor()
     y += 20
-    paddle_a.sety(y)
+    player.sety(y)
 
 def paddle_a_down():
-    y = paddle_a.ycor()
+    y = player.ycor()
     y -= 20
-    paddle_a.sety(y)
+    player.sety(y)
 
 
 
