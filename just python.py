92081d7de7543cb9521f8f5e36a79c912b74d726
Taha6400 <113461712+Taha6400@users.noreply.github.com>
2022-12-18 21:29:02 +0330

    Update just python.py

--- just python.py
+++ just python.py
@@ -1,2 +1,14 @@
-class hello():
-    self.y = y
+import turtle
+
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.tracer(0)
+
+# the player
+player = turtle.Turtle()
+player.color("white")
+player.shape("square")
+
+# main game loop
+while True:
+    wn.update()
